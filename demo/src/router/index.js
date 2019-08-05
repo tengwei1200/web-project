66fd22cbab6ab10d9db2d29761c2c7b1504e1930
tengwei1200 <273837914@qq.com>
2019-08-05 21:32:58 +0800

    8-5

--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -11,22 +11,22 @@ import MediaCar from '@/views/carTest/mediaCar'
 import ExhibiCar from '@/views/carTest/exhibiCar'
 
 Vue.use(Router)
-
 export default new Router({
   routes: [
-    { path: '/carMange', name: '车辆管理',component: Layout, meta: { keepAlive: false },children: [
-        { path: '/carMange/carCount', name: '车辆总表', component: CarCount, meta: {keepAlive: true} },
-        { path: '/carMange/secondCar', name: '过户车辆', component: SecondCar, meta: {keepAlive: true} },
-        { path: '/carMange/realName', name: '实名认证', component: RealName, meta: {keepAlive: true} },
-        { path: '/carMange/productCar', name: '产线车辆', component: ProductCar, meta: {keepAlive: true} },
-      ]
-    },
-    {
-      path: '/carTest', name:'车辆型号', component: Layout, meta: { keepAlive: false }, children: [
-        { path: '/carTest/testCar', name: '测试车', component: TestCar, meta: {keepAlive: true} },
-        { path: '/carTest/mediaCar', name: '媒体车', component: MediaCar, meta: {keepAlive: true} },
-        { path: '/carTest/exhibiCar', name: '展车', component: ExhibiCar, meta: {keepAlive: true} },
-      ] 
-    }
+      { path: '/', redirect: '/carMange' },
+      { path: '/carMange', name: '车辆管理',component: Layout, meta: { keepAlive: false },children: [
+          { path: '/carMange/carCount', name: '车辆总表', component: CarCount, meta: {keepAlive: true} },
+          { path: '/carMange/secondCar', name: '过户车辆', component: SecondCar, meta: {keepAlive: true} },
+          { path: '/carMange/realName', name: '实名认证', component: RealName, meta: {keepAlive: true} },
+          { path: '/carMange/productCar', name: '产线车辆', component: ProductCar, meta: {keepAlive: true} },
+        ]
+      },
+      {
+        path: '/carTest', name:'车辆型号', component: Layout, meta: { keepAlive: false }, children: [
+          { path: '/carTest/testCar', name: '测试车', component: TestCar, meta: {keepAlive: true} },
+          { path: '/carTest/mediaCar', name: '媒体车', component: MediaCar, meta: {keepAlive: true} },
+          { path: '/carTest/exhibiCar', name: '展车', component: ExhibiCar, meta: {keepAlive: true} },
+        ] 
+      }
   ]
 })
