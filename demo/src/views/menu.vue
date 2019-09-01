363128c295d36d8c6fec78a22a3df8cab02ba341
tengwei <18995833108@189.cn>
2019-09-01 23:25:26 +0800

    9-9

--- demo/src/views/menu.vue
+++ demo/src/views/menu.vue
@@ -14,44 +14,6 @@
                       </el-submenu>
                     </template>
                   </el-menu>
-                <!-- <el-menu
-                    default-active="2"
-                    class="el-menu-vertical-demo"
-                    @open="handleOpen"
-                    @close="handleClose"
-                    text-color="#000"
-                    active-text-color="#ffd04b">
-                    <el-submenu index="1">
-                        <template slot="title">
-                        <i class="el-icon-location"></i>
-                        <span>车辆管理</span>
-                        </template>
-                        <el-menu-item-group>
-                        <template slot="title">分组一</template>
-                        <el-menu-item index="1-1">车辆总表</el-menu-item>
-                        <el-menu-item index="1-2">实名认证</el-menu-item>
-                        </el-menu-item-group>
-                        <el-menu-item-group title="分组2">
-                        <el-menu-item index="1-3">过户车辆</el-menu-item>
-                        </el-menu-item-group>
-                        <el-submenu index="1-4">
-                        <template slot="title">产线车辆</template>
-                        <el-menu-item index="1-4-1">选项1</el-menu-item>
-                        </el-submenu>
-                    </el-submenu>
-                    <el-menu-item index="2">
-                        <i class="el-icon-menu"></i>
-                        <span slot="title">测试车</span>
-                    </el-menu-item>
-                    <el-menu-item index="3" disabled>
-                        <i class="el-icon-document"></i>
-                        <span slot="title">媒体车</span>
-                    </el-menu-item>
-                    <el-menu-item index="4">
-                        <i class="el-icon-setting"></i>
-                        <span slot="title">展车</span>
-                    </el-menu-item>
-                </el-menu> -->
             </el-col>
         </el-row>
     </div>
